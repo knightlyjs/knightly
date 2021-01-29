19453ff91db09b0963e4dbaad26a020355236a9f
Anthony Fu <anthonyfu117@hotmail.com>
2021-01-29 11:19:49 +0800

    fix: timeout for adding dist-tag

--- src/operations/publish.ts
+++ src/operations/publish.ts
@@ -4,6 +4,10 @@ import { NPM_TOKEN } from '../config'
 import { KnightlyJob, PackageInfo } from '../types'
 import { run } from '.'
 
+function timeout(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
 export async function publish({ dir, packageJSON }: PackageInfo, { publishTag, task: { defaultBranch = 'master' } }: KnightlyJob, dryRun = false) {
   // remove all scripts
   await fs.writeJSON(path.join(dir, 'package.json'), { ...packageJSON, scripts: {} }, { spaces: 2 })
@@ -13,6 +17,7 @@ export async function publish({ dir, packageJSON }: PackageInfo, { publishTag, t
   if (publishTag === defaultBranch && !dryRun) {
     const version = `${packageJSON.name}@${packageJSON.version}`
     try {
+      await timeout(2000)
       await run(`npm dist-tag add ${version} latest`, dir, { NODE_AUTH_TOKEN: NPM_TOKEN! })
     }
     catch (e) {
